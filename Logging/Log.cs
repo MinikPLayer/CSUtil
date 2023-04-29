ffd0733a14a0146d8541a570133a42cbe9dddc29
M. Tomecki <39161575+MinikPLayer@users.noreply.github.com>
2023-04-29 21:52:02 +0200

    Fixed Log not working in console mode

--- Logging/Log.cs
+++ Logging/Log.cs
@@ -7,24 +7,41 @@ namespace CSUtil.Logging
 {
     public static class Log
     {
+        static bool showDebugConsole = false;
+        static bool showConsle = false;
+        static Log()
+        {
+#if DEBUG
+            showDebugConsole = true;
+#else
+            showConsole = true;
+#endif
+        }
+
+        public static void ConfigureLogs(bool showDebug, bool showConsole)
+        {
+            Log.showDebugConsole = showDebug;
+            Log.showConsle = showConsole;
+        }
+
         public static void Normal(object data, bool newLine = true, ConsoleColor color = ConsoleColor.White, [CallerMemberName] string callerName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = -1)
         {
             string str = $"{filePath}:{lineNumber} |{callerName}| {data}";
             if (newLine)
                 str += "\n";
-#if DEBUG
-            System.Diagnostics.Debug.Write(str);
-#else
 
-            var bClr = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            /*Console.Write(filePath + "[" + callerName + "()");
-            if (lineNumber >= 0)
-                Console.Write(":" + lineNumber);
-            Console.Write("]");
-            Console.Write(data);*/
-            Console.ForegroundColor = bClr;
-#endif
+            if(showDebugConsole)
+            {
+                System.Diagnostics.Debug.Write(str);
+            }
+            
+            if(showConsle)
+            {
+                var bClr = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                Console.WriteLine(str);
+                Console.ForegroundColor = bClr;
+            }
         }
 
         public static void Warning(object data, bool newLine = true, [CallerMemberName] string callerName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = -1)
