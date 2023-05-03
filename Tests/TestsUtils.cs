d5f2622ef7a7003b6983f36c51f0efb4c97d7649
M. Tomecki <39161575+MinikPLayer@users.noreply.github.com>
2023-05-03 23:23:21 +0200

    Added ApiResult test utils

---
+++ Tests/TestsUtils.cs
@@ -0,0 +1,20 @@
+﻿using CSUtil.Web;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSUtil.Tests
+{
+    public static class TestsUtils
+    {
+        public static void Assert<T>(this ApiResult<T> result, bool checkContent = true)
+        {
+            NUnit.Framework.Assert.IsTrue(result.IsOk(), result.StatusCode + " - " + result.Message);
+            if (checkContent)
+                NUnit.Framework.Assert.NotNull(result.Payload);
+        }
+    }
+}
