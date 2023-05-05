be2e74b7de890939076345a6688c2f2d15badd1a
M. Tomecki <39161575+MinikPLayer@users.noreply.github.com>
2023-05-05 09:49:10 +0200

    Improved SortedLinkedList speed by adding option to AddRange instead of single Add

--- Data/SortedLinkedList.cs
+++ Data/SortedLinkedList.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -28,12 +29,24 @@ namespace CSUtil.Data
             }
         }
 
+        public int Count => list.Count;
+
         public IEnumerator<T> GetEnumerator() => list.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => list.GetEnumerator();
 
-        void ForceReorder()
+        public void ForceReorder()
         {
             list = new LinkedList<T>(Inverted ? list.OrderByDescending(x => x) : list.OrderBy(x => x));
+        }
+
+        public void Clear() => list.Clear();
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            foreach (var o in items)
+                list.AddFirst(o);
+
+            ForceReorder();
         }
 
         public void Add(T item)
@@ -58,6 +71,10 @@ namespace CSUtil.Data
 
             list.AddAfter(lastNode, item);
         }
+
+        public SortedLinkedList() { }
+
+        public SortedLinkedList(IEnumerable<T> data) => AddRange(data);
     }
 
     public class SortedLinkedListTests
@@ -98,28 +115,133 @@ namespace CSUtil.Data
             Assert.That(finalList, Is.EqualTo(orderedList));
         }
 
+        const int TEST_SIZE = 10_000;
         [Test]
         public void InvertTest()
         {
-            const int size = 10_000;
-
-            var randomList = new List<int>(size);
+            var randomList = new List<int>(TEST_SIZE);
             var sortedList = new SortedLinkedList<int>();
             sortedList.Inverted = false;
 
-            for (var i = 0; i < size; i++)
+            for (var i = 0; i < TEST_SIZE; i++)
             {
                 var rnd = Random.Shared.Next();
                 randomList.Add(rnd);
                 sortedList.Add(rnd);
 
-                if (i == size / 2)
+                if (i == TEST_SIZE / 2)
                     sortedList.Inverted = true;
             }
 
             var orderedList = randomList.OrderByDescending(x => x);
             var finalList = sortedList.ToList();
             Assert.That(finalList, Is.EqualTo(orderedList));
+        }
+
+        [Test]
+        public void TestAddRange()
+        {
+            var randomList = new List<int>(TEST_SIZE);
+            for (var i = 0; i < TEST_SIZE; i++)
+                randomList.Add(i);
+
+            var sortedList = new SortedLinkedList<int>();
+            sortedList.AddRange(randomList);
+            Assert.That(sortedList.Count, Is.EqualTo(TEST_SIZE));
+
+            sortedList.AddRange(randomList);
+            Assert.That(sortedList.Count, Is.EqualTo(TEST_SIZE * 2));
+
+            var enumerator = sortedList.GetEnumerator();
+            for(var i = 0; i < TEST_SIZE; i++)
+            {
+                enumerator.MoveNext();
+                var i1 = enumerator.Current;
+                enumerator.MoveNext();
+                var i2 = enumerator.Current;
+
+                Assert.That(i1, Is.EqualTo(i2));
+            }
+        }
+
+        [Test]
+        public void TimeTestNonInverted()
+        {
+            var list = new SortedLinkedList<int>();
+            var time1 = DateTime.Now;
+            for (var i = 0; i < TEST_SIZE; i++)
+                list.Add(i);
+            var time2 = DateTime.Now;
+
+            var logBuilder = new StringBuilder();
+            logBuilder.Append("Time for ");
+            logBuilder.Append(TEST_SIZE);
+            logBuilder.Append(" projects: ");
+            logBuilder.Append((time2 - time1).TotalSeconds);
+            logBuilder.Append("s");
+
+            Console.WriteLine(logBuilder.ToString());
+        }
+
+        [Test]
+        public void TimeTestInverted()
+        {
+            var list = new SortedLinkedList<int>();
+            var time1 = DateTime.Now;
+            for (var i = 0; i < TEST_SIZE; i++)
+                list.Add(TEST_SIZE - i);
+            var time2 = DateTime.Now;
+
+            var logBuilder = new StringBuilder();
+            logBuilder.Append("Time for ");
+            logBuilder.Append(TEST_SIZE);
+            logBuilder.Append(" projects: ");
+            logBuilder.Append((time2 - time1).TotalSeconds);
+            logBuilder.Append("s");
+
+            Console.WriteLine(logBuilder.ToString());
+        }
+
+        [Test]
+        public void TimeTestRandom()
+        {
+            var list = new SortedLinkedList<int>();
+            var time1 = DateTime.Now;
+            for (var i = 0; i < TEST_SIZE; i++)
+                list.Add(Random.Shared.Next());
+
+            var time2 = DateTime.Now;
+
+            var logBuilder = new StringBuilder();
+            logBuilder.Append("Time for ");
+            logBuilder.Append(TEST_SIZE);
+            logBuilder.Append(" projects: ");
+            logBuilder.Append((time2 - time1).TotalSeconds);
+            logBuilder.Append("s");
+
+            Console.WriteLine(logBuilder.ToString());
+        }
+
+        [Test]
+        public void TimeTestAddRangeNonInverted()
+        {
+            var list = new List<int>(TEST_SIZE);
+            var sortedList = new SortedLinkedList<int>();
+            var time1 = DateTime.Now;
+            for (var i = 0; i < TEST_SIZE; i++)
+                list.Add(i);
+
+            sortedList.AddRange(list);
+            var time2 = DateTime.Now;
+
+            var logBuilder = new StringBuilder();
+            logBuilder.Append("Time for ");
+            logBuilder.Append(TEST_SIZE);
+            logBuilder.Append(" projects: ");
+            logBuilder.Append((time2 - time1).TotalSeconds);
+            logBuilder.Append("s");
+
+            Console.WriteLine(logBuilder.ToString());
         }
     }
 }
